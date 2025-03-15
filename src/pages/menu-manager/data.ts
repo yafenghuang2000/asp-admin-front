cfb03d8da124b457a1a68669da06a3024c3e57e9
yafenghuang2000 <yafenghuang2000@gmail.com>
2025-03-15 23:24:48 +0800

    feat(menu-manager): 优化菜单管理界面及数据结构

--- src/pages/menu-manager/data.ts
+++ src/pages/menu-manager/data.ts
@@ -1,6 +1,6 @@
 export const createMenu = (): Array<{ id: number; title: string }> => {
   const list = [];
-  for (let i = 0; i < 40; i++) {
+  for (let i = 0; i < 20; i++) {
     list.push({ id: i + 1, title: `菜单名称${i + 1}` });
   }
   return list;
@@ -11,4 +11,16 @@ export interface ISOnSelectNodeProps {
   key: string;
   title: string;
   path: string;
+  children?: Array<ISMenuDetail> | null;
+}
+
+export interface ISMenuDetail {
+  key: string;
+  title: string;
+  path?: string;
+  code: string | null;
+  type: string | null;
+  icon: string | null;
+  sort: string | number | null;
+  remark: string | null;
 }
