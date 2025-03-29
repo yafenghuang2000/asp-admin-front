d014d2dfe6a574801f2c1c6f66c4ed638ac8f1de
yafenghuang2000 <yafenghuang2000@gmail.com>
2025-03-29 20:09:39 +0800

    refactor(vite.config): 更新分包策略，简化node_modules模块处理逻辑并启用原始文件删除

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import fs from 'fs';
 import legacy from '@vitejs/plugin-legacy';
 import stylelint from 'vite-plugin-stylelint';
 import { viteMockServe } from 'vite-plugin-mock';
@@ -35,7 +34,7 @@ export default defineConfig(({ mode }) => {
         algorithm: 'gzip', // 使用 gzip 压缩
         ext: '.gz', // 生成的文件扩展名
         threshold: 1024 * 300, // 对大于 10KB 的文件进行压缩
-        deleteOriginFile: false, // 是否删除原始文件
+        deleteOriginFile: true, // 是否删除原始文件
       }),
     ],
     server: {
@@ -82,39 +81,23 @@ export default defineConfig(({ mode }) => {
       rollupOptions: {
         output: {
           manualChunks: (id) => {
+            // 检查是否是 node_modules 中的模块
             if (id.includes('node_modules')) {
-              // 确保路径中没有空字节和查询参数
-              const cleanId = id.replace(/\0/g, '').split('?')[0];
-              // 获取文件大小（以字节为单位）
-              const fileSize = fs.statSync(cleanId).size;
-              const ONE_MB = 500 * 1024; // 500KB
-
-              // 将 react 和 react-dom 分开打包
-              if (cleanId.includes('react')) {
-                return fileSize > ONE_MB ? 'react-large' : 'react';
-              }
-              if (cleanId.includes('react-dom')) {
-                return fileSize > ONE_MB ? 'react-dom-large' : 'react-dom';
-              }
-
-              // 处理 antd 相关包
-              if (cleanId.includes('@ant-design') || cleanId.includes('antd')) {
-                return fileSize > ONE_MB ? 'antd-vendor-large' : 'antd-vendor';
-              }
-
-              // 处理其他包
-              if (fileSize > ONE_MB) {
-                const pkgName = cleanId
-                  .toString()
-                  .split('node_modules/')[1]
-                  .split('/')[0]
-                  .toString();
-                return `vendor-large-${pkgName.replace('@', '').replace('/', '_')}`;
-              }
-              return null;
+              return 'vendor'; // 将所有来自 node_modules 的包放入 vendor 分包
+            }
+            // 根据项目结构进行分包
+            if (id.includes('src/utils')) {
+              return 'utils'; // 将所有来自 src/utils 的包放入 utils 分包
+            }
+            if (id.includes('src/components')) {
+              return 'components'; // 将所有来自 src/components 的包放入 components 分包
             }
 
-            return null;
+            if (id.includes('src/pages')) {
+              return 'pages';
+            }
+            // 其他模块可以根据需要进行分包
+            return null; // 返回 null 以使用默认分包
           },
           chunkFileNames: 'js/[name].[hash].js',
           entryFileNames: 'js/[name].[hash].js',
